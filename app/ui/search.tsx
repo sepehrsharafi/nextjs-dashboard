7fc31942d51681daca8fd6d73762aa963b018186
sepehrsharafi <sepehr.sharafi123@gmail.com>
2025-04-01 23:47:03 +0330

    Fixed type errors

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,7 +3,7 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
 
-export default function Search() {
+export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
@@ -27,7 +27,7 @@ export default function Search() {
       </label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={"Search invoices..."}
+        placeholder={placeholder}
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
